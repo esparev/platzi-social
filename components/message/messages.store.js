17f50957021b467ebf3376b2a7d0b4a8baade6c9
JoseMa <esparev@hotmail.com>
2023-03-23 15:41:21 -0600

    feat: ✨ store and read data from the db

--- components/message/messages.store.js
+++ components/message/messages.store.js
@@ -1,11 +1,21 @@
-const list = [];
+const db = require('mongoose');
+const MessageModel = require('./messages.model');
 
-function getMessages() {
-  return list;
+db.Promise = global.Promise;
+db.set('strictQuery', false);
+db.connect('mongodb://root:admin@localhost:27017/?authMechanism=DEFAULT', {
+  useNewUrlParser: true,
+});
+
+console.log('[db] Successfully connected');
+
+async function getMessages() {
+  return MessageModel.find();
 }
 
 function addMessage(message) {
-  list.push(message);
+  const myMessage = new MessageModel(message);
+  myMessage.save();
 }
 
 module.exports = { list: getMessages, add: addMessage };
